b02f42ddb764ec655d1065bd22fbcd8be8f8ac4c
Dimitris Mitsiou <dmitsiou@vmware.com>
2021-10-09 13:03:37 +0300

    add grids to results

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "./styles.css";
 import { fromEvent } from "rxjs";
 import { debounceTime } from "rxjs/operators";
 
-let appendToResults = (result) => {
-  const resultsContainer = document.getElementById("results");
+let appendToResults = (result, id) => {
+  const resultsContainer = document.getElementById(id);
   const span = document.createElement("span");
   span.innerHTML = result;
   span.classList = ['result']
@@ -14,19 +14,19 @@ let appendToResults = (result) => {
 const button = document.getElementById("hit-me");
 
 // Vanilla JS event listener
-button.addEventListener("click", appendToResults("Vanilla"));
+button.addEventListener("click", appendToResults("Vanilla", 'vanilla-results'));
 // button.removeEventListener("click", appendToResults("Vanilla"));
 
 // RxJS event listener
 const subscription1 = fromEvent(button, "click").subscribe(() =>
-  appendToResults("RxJS")
+  appendToResults("RxJS", 'rxjs-results')
 );
 
 // RxJS event listener with operators
 fromEvent(button, "click")
   .pipe(debounceTime(1000))
   .subscribe(() => {
-    appendToResults("RxJS with delay");
+    appendToResults("RxJS with delay", 'rxjs-delay-results');
 
     // clean up with unsubscribe
     // subscription1.unsubscribe();
