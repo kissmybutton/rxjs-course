8888392150f0d07b4b24e53506bf58f8d6f4f46b
Dimitris Mitsiou <dmitsiou@vmware.com>
2021-10-09 20:16:32 +0300

    add operators functions

--- src/index.js
+++ src/index.js
@@ -1,41 +1,89 @@
 import "./styles.css";
-import { Observable } from "rxjs";
+import { of, interval, merge } from "rxjs";
+import {
+  map,
+  filter,
+  tap,
+  catchError,
+  takeUntil,
+  take,
+  mergeMap
+} from "rxjs/operators";
 
-let appendToResults = (result, id = 'results') => {
+let appendToResults = (result, id = "results") => {
   const resultsContainer = document.getElementById(id);
   const span = document.createElement("span");
   span.innerHTML = result;
-  span.classList = ['result']
+  span.classList = ["result"];
   resultsContainer.append(span);
   const br = document.createElement("br");
   resultsContainer.append(br);
 };
 
-// Observable
-const observable = new Observable((subscriber) => {
-  subscriber.next(1);
-  subscriber.next(2);
-  subscriber.next(3);
-  setTimeout(() => {
-    subscriber.next(4);
-    subscriber.complete();
-    subscriber.next(5); // Is not delivered because it would violate the contract
-  }, 1000);
-});
-appendToResults("just before subscribe");
+// Map
+of(1, 2, 3)
+  .pipe(map((x) => x * x))
+  .subscribe((v) => appendToResults(`value: ${v}`, "results-container-map"));
 
-// Observer
-const observer = {
-  next(x) {
-    appendToResults("next " + x);
-  },
-  error(err) {
-    appendToResults("error " + err);
-  },
-  complete() {
-    appendToResults("complete");
-  }
-};
-observable.subscribe(observer);
+// Filter
+of(1, 2, 3, 4, 5, 6, 7)
+  .pipe(filter((x) => x % 2 === 0))
+  .subscribe((v) => appendToResults(`value: ${v}`, "results-container-filter"));
 
-appendToResults("just after subscribe");
+// Map + Filter
+of(0, 1, 2, 3, 4, 5, 6, 7)
+  .pipe(
+    map((x) => x + 1),
+    filter((x) => x % 2 === 0)
+    // take(2),
+  )
+  .subscribe((v) => appendToResults(`value: ${v}`, "results-container-combo"));
+
+// Error
+interval(1000)
+  .pipe(
+    map((x) => {
+      if (x > 0 && x % 5 === 0) {
+        return new Error(`Error mapping value ${x}`);
+      }
+      return x;
+    }),
+    catchError((err) => {
+      appendToResults(`Catch error worked`, "results-container-error");
+      return of(`observable derived from error ${err}`);
+    })
+  )
+  .subscribe(
+    (v) => appendToResults(`subcriber: ${v}`, "results-container-error"),
+    (err) => appendToResults(`error: ${err}`, "results-container-error")
+  );
+
+// Merge
+const observable1 = interval(1000).pipe(
+  take(7),
+  map(() => "Observable 1")
+);
+const observable2 = interval(3000).pipe(
+  take(2),
+  map(() => "Observable 2")
+);
+merge(observable1, observable2)
+  .pipe(map((x) => x + " emited"))
+  .subscribe((v) =>
+    appendToResults(`subcriber: ${v}`, "results-container-merge")
+  );
+
+// MergeMap
+const letters = of("a", "b", "c");
+const result = letters.pipe(
+  mergeMap((x) => {
+    appendToResults(`${x} emitted`, "results-container-mergemap");
+    return interval(1000).pipe(
+      take(5),
+      map((i) => x + i)
+    );
+  })
+);
+result.subscribe((x) =>
+  appendToResults(`${x} emitted`, "results-container-mergemap")
+);
