0723fda1ee90d112ba24ff865d8884766f6c2f7a
Dimitris Mitsiou <dmitsiou@vmware.com>
2021-11-05 16:28:37 +0200

    change event listener

--- src/index.js
+++ src/index.js
@@ -6,28 +6,38 @@ let appendToResults = (result, id) => {
   const resultsContainer = document.getElementById(id);
   const span = document.createElement("span");
   span.innerHTML = result;
-  span.classList = ['result']
+  span.classList = ["result"];
   resultsContainer.append(span);
   const br = document.createElement("br");
   resultsContainer.append(br);
 };
 const button = document.getElementById("hit-me");
 
+const appendToResultsFn = appendToResults.bind(
+  this,
+  "Vanilla",
+  "vanilla-results"
+);
+
 // Vanilla JS event listener
-button.addEventListener("click", appendToResults("Vanilla", 'vanilla-results'));
-// button.removeEventListener("click", appendToResults("Vanilla"));
+button.addEventListener("click", appendToResultsFn);
+setTimeout(() => {
+  // button.removeEventListener("click", appendToResultsFn);
+}, 3000);
 
 // RxJS event listener
 const subscription1 = fromEvent(button, "click").subscribe(() =>
-  appendToResults("RxJS", 'rxjs-results')
+  appendToResults("RxJS", "rxjs-results")
 );
+
+setTimeout(() => {
+  // clean up with unsubscribe
+  // subscription1.unsubscribe();
+}, 3000);
 
 // RxJS event listener with operators
 fromEvent(button, "click")
   .pipe(debounceTime(1000))
   .subscribe(() => {
-    appendToResults("RxJS with delay", 'rxjs-delay-results');
-
-    // clean up with unsubscribe
-    // subscription1.unsubscribe();
+    appendToResults("RxJS with delay", "rxjs-delay-results");
   });
