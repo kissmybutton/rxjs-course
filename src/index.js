00617e638222986f29a75d68bc9b61ecb29ed93e
dimitrismitsiou <dimitrismitsiou@outlook.com>
2021-11-16 14:40:46 +0200

    update error functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { of, interval, merge } from "rxjs";
+import { of, interval, merge, throwError } from "rxjs";
 import {
   map,
   filter,
@@ -7,7 +7,9 @@ import {
   catchError,
   takeUntil,
   take,
-  mergeMap
+  mergeMap,
+  finalize,
+  debounceTime
 } from "rxjs/operators";
 
 let appendToResults = (result, id = "results") => {
@@ -34,56 +36,73 @@ of(1, 2, 3, 4, 5, 6, 7)
 of(0, 1, 2, 3, 4, 5, 6, 7)
   .pipe(
     map((x) => x + 1),
-    filter((x) => x % 2 === 0)
-    // take(2),
+    filter((x) => x % 2 === 0),
+    take(2)
   )
   .subscribe((v) => appendToResults(`value: ${v}`, "results-container-combo"));
 
-// Error
-interval(1000)
+// Error example 1 - Handle error inside the pipe
+throwError(1)
   .pipe(
-    map((x) => {
-      if (x > 0 && x % 5 === 0) {
-        return new Error(`Error mapping value ${x}`);
-      }
-      return x;
-    }),
     catchError((err) => {
       appendToResults(`Catch error worked`, "results-container-error");
       return of(`observable derived from error ${err}`);
+    }),
+    finalize((data) => {
+      appendToResults(`Finalize worked`, "results-container-error");
+      return throwError(`observable derived from error ${data}`);
     })
   )
   .subscribe(
-    (v) => appendToResults(`subcriber: ${v}`, "results-container-error"),
-    (err) => appendToResults(`error: ${err}`, "results-container-error")
+    (v) => appendToResults(`next: ${v}`, "results-container-error"),
+    (err) => appendToResults(`error: ${err}`, "results-container-error"),
+    () => appendToResults(`complete`, "results-container-error")
   );
 
-// Merge
-const observable1 = interval(1000).pipe(
-  take(7),
-  map(() => "Observable 1")
-);
-const observable2 = interval(3000).pipe(
-  take(2),
-  map(() => "Observable 2")
-);
-merge(observable1, observable2)
-  .pipe(map((x) => x + " emited"))
-  .subscribe((v) =>
-    appendToResults(`subcriber: ${v}`, "results-container-merge")
-  );
+// Error example 2 - Handle the error and rethrow it
+// throwError(2)
+//   .pipe(
+//     catchError((err) => {
+//       appendToResults(`Catch error worked`, "results-container-error");
+//       return throwError(`observable derived from error ${err}`);
+//     }),
+//     finalize((data) => {
+//       appendToResults(`Finalize worked`, "results-container-error");
+//       return throwError(`observable derived from error ${data}`);
+//     })
+//   )
+//   .subscribe(
+//     (v) => appendToResults(`next: ${v}`, "results-container-error"),
+//     (err) => appendToResults(`error: ${err}`, "results-container-error"),
+//     () => appendToResults(`complete`, "results-container-error")
+//   );
 
-// MergeMap
-const letters = of("a", "b", "c");
-const result = letters.pipe(
-  mergeMap((x) => {
-    appendToResults(`${x} emitted`, "results-container-mergemap");
-    return interval(1000).pipe(
-      take(5),
-      map((i) => x + i)
-    );
-  })
-);
-result.subscribe((x) =>
-  appendToResults(`${x} emitted`, "results-container-mergemap")
-);
+// // Merge
+// const observable1 = interval(1000).pipe(
+//   take(7),
+//   map(() => "Observable 1")
+// );
+// const observable2 = interval(3000).pipe(
+//   take(2),
+//   map(() => "Observable 2")
+// );
+// merge(observable1, observable2)
+//   .pipe(map((x) => x + " emited"))
+//   .subscribe((v) =>
+//     appendToResults(`next: ${v}`, "results-container-merge")
+//   );
+
+// // MergeMap
+// const letters = of("a", "b", "c");
+// const result = letters.pipe(
+//   mergeMap((x) => {
+//     appendToResults(`${x} emitted`, "results-container-mergemap");
+//     return interval(1000).pipe(
+//       take(5),
+//       map((i) => x + i)
+//     );
+//   })
+// );
+// result.subscribe((x) =>
+//   appendToResults(`${x} emitted`, "results-container-mergemap")
+// );
